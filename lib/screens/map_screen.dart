c73cb6bffb639ec099b3c1a2da3e4b552482a03c
oswaldo olea <o.olea@sintraconsulting.com>
2023-03-25 10:47:24 +0100

    cleared MapScreen, ApplicationState, LoginAccount, Authentication files

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,8 +1,7 @@
-//import 'dart:ffi';
+
 
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:flutter_black_white/utils/shared_preferences.dart';
+
 
 
 class MapScreen extends StatefulWidget {
@@ -15,48 +14,7 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapScreen> {
 
-  late GoogleMapController mapController;
 
-  //final LatLng _center = const LatLng(43.651070, -79.347015);
-  final LatLng _center = LatLng(Preferences.locationLat,Preferences.locationLng);
-
-  //final Uint8List markerIcon = await getBytesFromAsset('assets/images/evs.png', 100);
-  late BitmapDescriptor mapMarker;
-
-  @override
-  void initState() {
-    super.initState();
-    setCustomMarker();
-  }
-
-  void setCustomMarker() async {
-    mapMarker = await BitmapDescriptor.fromAssetImage(const ImageConfiguration(), 'assets/map_marker.png');
-  }
-
-  /* final Set<Marker> _markers = {
-    const Marker(
-      markerId: MarkerId('defaultLocation'),
-      position: LatLng(37.4689, -122.1424),
-      //icon: BitmapDescriptor.defaultMarker,
-      icon: const mapMarker
-    )
-  }; */
-
-  final Set<Marker> _markers = {};  
-
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-
-    setState(() {
-      _markers.add(
-        Marker(
-          markerId: const MarkerId('defaultLocation'),
-          position: const LatLng(37.4689, -122.1424),
-          icon: mapMarker
-        )
-      );
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -87,14 +45,7 @@ class _MapScreenState extends State<MapScreen> {
           const SizedBox( width: 10,)         
         ],
       ),
-      body: GoogleMap(
-        onMapCreated: _onMapCreated,
-        initialCameraPosition: CameraPosition(
-          target: _center,
-          zoom: 11.0,
-        ),
-        markers: _markers,
-      ),
+
     );
   }
 }
