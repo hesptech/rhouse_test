8744924c3f3e8ae3de209201d8369bc102c90c88
oswaldo olea <o.olea@sintraconsulting.com>
2023-05-19 13:07:05 +0200

    refactor: providers

--- lib/providers/repliers_listing_mls.dart
+++ lib/providers/repliers_listing_mls.dart
@@ -12,7 +12,9 @@ class RepliersListingMls extends ChangeNotifier {
 
   Future<String> _getJsonData( dynamic endPoint ) async {
 
-    final url = Uri.https( kBaseUrl, endPoint);
+    final url = Uri.https( kBaseUrl, endPoint, {
+      'fields': 'history',
+    });
     //print( url );
 
     Map<String, String>? headers = { 'REPLIERS-API-KEY': dotenv.get('REPLIERS-API-KEY') };
