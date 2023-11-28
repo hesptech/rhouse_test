10d93350b12413fd78653e7eda04c7ae0c1bd693
Jesus <jesusmarquez.mc@gmail.com>
2023-11-27 20:07:30 -0600

    fix: remove print
    Remove print data

--- lib/providers/account/session_provider.dart
+++ lib/providers/account/session_provider.dart
@@ -26,8 +26,6 @@ class SessionProvider {
         return Future.value(true);
       }
 
-      print(userData);
-
       var expiresInDate = DateTime.parse(expiresIn);
 
       if (expiresInDate.isBefore(DateTime.now().toUtc())) {
